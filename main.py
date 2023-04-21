55b590c039bd44be7d26e86fbb85c5b49db91da4
Hugo Grimmett <hugo.grimmett@gmail.com>
2023-04-21 10:58:10 +0200

    two big improvements. Firstly I close the requests, so it continues working after 9 light commands. Secondly, the button debouncing is better and it picks up button presses very nicely

--- main.py
+++ main.py
@@ -9,23 +9,6 @@ import os
 # credentials = "zuMLxhoETtzzWnIjlQidaQx-IXLkKsBTaEOaUM9n"
 # light_index = 1
 
-# pin_led = machine.Pin(2, machine.Pin.OUT)
-# pin_switch = machine.Pin(13, machine.Pin.IN, machine.Pin.PULL_DOWN)
-# r = urequests.request('GET','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index))
-# light_state = r.json()["state"]["on"]
-
-# print('Scanning for button presses...')
-# while (True):
-#     if pin_switch.value() == 1:
-#         # print('button is on')
-#         if pin_switch.value() == 0:
-#             print('Button pressed and released')
-#             # print('button is off')
-#             r = urequests.request('GET','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index))
-#             light_state = r.json()["state"]["on"]
-#             print('   light state was %s...' % light_state)
-#             r = urequests.request('PUT','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index) + '/state','{"on":%s}' % str(not light_state).lower())
-#             print('   ... switching to %s' % (not light_state))
 
 # turn light on
 # r = urequests.request('PUT','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index) + '/state','{"on":true}')
@@ -91,15 +74,19 @@ def get_new_credentials(device_name, hue_bridge_ip_address):
         credentials = r.json()[0]['success']['username']
     except TypeError:
         print('ERROR: could not parse Hue bridge response')
+        r.close()
         return False
+    r.close()
     return credentials
 
 def test_credentials(hue_bridge_ip_address, credentials):
     r = urequests.request('GET','http://' + hue_bridge_ip_address + '/api/'+ credentials)
     try:
         r.json()[0]["error"]["description"]
+        r.close()
     except KeyError:
         # this is the good outcomel, because the error field doesn't exist
+        print('   ...credentials pass')
         return True
     except:
         print('ERROR: in test_credentials. Something else went wrong')
@@ -113,23 +100,47 @@ def test_credentials(hue_bridge_ip_address, credentials):
 def test_hue_bridge_connection(hue_bridge_ip_address):
     try:
         r = urequests.request('GET','http://' + hue_bridge_ip_address + '/api/newdeveloper')
+        r.close()
     except OSError:
         print('ERROR: cannot connect to Hue bridge on given IP address: ' + hue_bridge_ip_address)
         return False
+    print('   ...bridge connection passes')
     return True
-
-
 
 saved_info_file_name = 'hue_ip_credentials.txt'
 light_index = 1
 hue_bridge_ip_address, device_name, credentials = get_ip_device_credentials(saved_info_file_name)
-# flash the light on and off
-r = urequests.request('PUT','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index) + '/state','{"on":true}')
-time.sleep_ms(1000)
-r = urequests.request('PUT','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index) + '/state','{"on":false}')
+print('Connected to bridge!')
+# # flash the light on and off
+# r = urequests.request('PUT','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index) + '/state','{"on":true}')
+# time.sleep_ms(1000)
+# r = urequests.request('PUT','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index) + '/state','{"on":false}')
 
-# def is_hue_bridge_connected(hue_bridge_ip_address):
-#     # send test message
-#     try: 
-#         r = urequests.request('GET','http://'+hue_bridge_ip_address+'/api/newdeveloper')
+pin_led = machine.Pin(2, machine.Pin.OUT)
+pin_switch = machine.Pin(13, machine.Pin.IN, machine.Pin.PULL_DOWN)
+r = urequests.request('GET','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index))
+light_state = r.json()["state"]["on"]
+r.close()
 
+print('Scanning for button presses...')
+while True:
+    first = pin_switch.value()
+    time.sleep_ms(10)
+    second = pin_switch.value()
+    if second and not first:
+        print('Button pressed!')
+    elif first and not second:
+        print('Button released!')
+        # for _ in range(2):
+        #     pin_led = 1
+        #     time.sleep_ms(100)
+        #     pin_led = 0
+        #     time.sleep_ms(100)
+        r = urequests.request('GET','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index))
+        light_state = r.json()["state"]["on"]
+        r.close()
+        print('   light state was %s...' % light_state)
+        r = urequests.request('PUT','http://' + hue_bridge_ip_address + '/api/' + credentials + '/lights/' + str(light_index) + '/state','{"on":%s}' % str(not light_state).lower())
+        print('   ... switching to %s' % (not light_state))
+        r.close()
+            
